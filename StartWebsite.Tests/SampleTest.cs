b3b320a56946384b25db821b59aeb46fe3e483bc
Pavan Banakar <pbanakar@canadagoose.com>
2020-03-11 20:50:50 -0400

    Added Unit Tests

---
+++ StartWebsite.Tests/SampleTest.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StartWebsite.Tests
+{
+    [TestClass]
+    public class SampleTest
+    {
+        [TestMethod]
+        public void TestTrue()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        public void TestFalse()
+        {
+            Assert.AreNotEqual(1, 1);
+        }
+    }
+}
